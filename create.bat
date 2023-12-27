111b3e4584388f4450115a2db2ff0be915024203
jim-miller-gac <jim.miller.gac@gmail.com>
2023-12-27 14:31:54 -0600

    another update

---
+++ create.bat
@@ -0,0 +1,8 @@
+echo "# jim-miller-gac" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jim-miller-gac/jim-miller-gac.git
+git push -u origin main
+
